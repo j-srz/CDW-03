ea85fb6fc620ee64f63cd9c832ae3e3fc1a4f46a
Jes <suarezliceajesus@gmail.com>
2025-01-11 23:05:53 -0600

    Agregue funcionalidad de actualizar

--- admin/index.php
+++ admin/index.php
@@ -1,4 +1,16 @@
 <?php
+
+
+
+require '../includes/config/database.php';
+$db = conectarDB();
+
+$query = "SELECT * FROM propiedades";
+
+$resultadoConsulta = mysqli_query($db, $query);
+
+
+
 $resultado = $_GET['resultado'] ?? null;
 
 require '../includes/funciones.php';
@@ -9,14 +21,47 @@ incluirTemplate('header');
     <h1>Administrador de bienes raices</h1>
     <?php if (intval($resultado) === 1) : ?>
         <p class="alerta exito">Anuncio creado correctamente</p>
+    <?php elseif (intval($resultado) === 2) : ?>
+        <p class="alerta exito">Anuncio actualizado correctamente</p>
+    <?php elseif (intval($resultado) === 3) : ?>
+        <p class="alerta exito">Anuncio eliminado correctamente</p>
     <?php endif; ?>
 
     <a href="/admin/propiedades/crear.php" class="boton boton-verde">Nueva Propiedad</a>
 
+
+    <table class="propiedades">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Título</th>
+                <th>Imagen</th>
+                <th>Precio</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php while($propiedad = mysqli_fetch_assoc($resultadoConsulta)) :?>
+                <tr>
+                <td><?php echo $propiedad['id']; ?></td>
+                <td><?php echo $propiedad['titulo']; ?></td>
+                <td> <img src="/imagenes/<?php echo $propiedad['imagen']; ?>" alt="Imagen propiedad" class="imagen-tabla"></td>
+                <td>$<?php echo $propiedad['precio']; ?></td>
+                <td>
+                    <a href="" class="boton-rojo-block">Eliminar</a>
+                    <a href="admin/propiedades/actualizar.php?id=<?php echo $propiedad['id']; ?>" class="boton-amarillo-block">Actualizar</a>
+                </td>
+            </tr>
+
+            <?php endwhile; ?>
+        </tbody>
+    </table>
 </main>
 
 
 <?php
+
+mysqli_close($db);
 
 incluirTemplate('footer');
 ?>
